937c7d8171e439865dfea332a46a400caab703f4
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2023-12-16 12:50:17 -0300

    Repaso JS

---
+++ Clase1_16-12/index.js
@@ -0,0 +1,17 @@
+console.log("Inicio curso Coderhouse");
+
+console.warn("Advertencia");
+
+console.info("Info");
+
+console.error("ERROR");
+
+console.log(1);
+console.log(true);
+console.log(null);
+console.log(undefined);
+console.log([1,2,3,4,5]);
+console.log({
+    nombre: "Juan",
+    apellido: "Ronquillo"
+});
