c4b608d172857da3208ec753b151f8a400b29f1b
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2023-12-26 18:46:06 -0300

    Desafío Resuelto con id corregido

--- Desafíos/1-Clases-Con-ECMAScript.js
+++ Desafíos/1-Clases-Con-ECMAScript.js
@@ -55,14 +55,9 @@ class ProductManager {
                 price,
                 thumbnail,
                 code,
-                stock
+                stock,
+                id: this.products.length + 1
             }
-
-            if (this.products.length === 0) {
-                product.id = 1;
-                } else {
-                product.id = this.products.length + 1;
-                }
 
         this.products.push(product);
     }
